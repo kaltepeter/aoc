458f9ff93b6e1cb6fff0f168f00f2f73ba596c0c
kaltepeter <kayla@kaylaaltepeter.com>
2019-12-01 11:31:11 -0600

    config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import {of, from, pipe, range} from 'rxjs';
-import {toArray, map, concat} from 'rxjs/operators';
+import { from, of, pipe, range } from 'rxjs';
+import { concat, map, toArray } from 'rxjs/operators';
 
 const fuelRequired = (mass: number) => {
   return Math.floor(mass / 3) - 2;
-}
+};
 
 // const numbers = range(0, 40).pipe(
 //   concat(of(9.4)),
@@ -14,13 +14,15 @@ const fuelRequired = (mass: number) => {
 
 const vals = [
   [12, 2],
-  [14,2],
+  [14, 2],
   [1969, 654],
   [100756, 33583]
-  ];
+];
 
-const numbers = from(vals).pipe(
-  map(num => {
-    console.log(`num: ${num}, value: ${fuelRequired(num)}`)
-  })
-).subscribe();
+const numbers = from(vals)
+  .pipe(
+    map(num => {
+      console.log(`num: ${num}, value: ${fuelRequired(num)}`);
+    })
+  )
+  .subscribe();

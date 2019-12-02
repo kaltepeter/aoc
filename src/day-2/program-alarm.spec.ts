fe9851c01f84a0d9793603e4d007d0a1e28c676f
kaltepeter <kayla@kaylaaltepeter.com>
2019-12-02 01:07:17 -0600

    start ops

--- src/day-2/program-alarm.spec.ts
+++ src/day-2/program-alarm.spec.ts
@@ -1,26 +1,82 @@
-import { execGravityAssistProgram } from './program-alarm';
+import {
+  execGravityAssistProgram,
+  execOp,
+  fns,
+  getOpFn
+} from './program-alarm';
 
 describe('2: program alarm', () => {
-  describe.each([
+  describe.skip.each([
     [
-      [1, 0, 0, 0, 99],
-      [2, 0, 0, 0, 99]
-    ],
-    [
-      [2, 3, 0, 3, 99],
-      [2, 3, 0, 6, 99]
-    ],
-    [
-      [2, 4, 4, 5, 99],
-      [2, 4, 4, 5, 99, 9801]
-    ],
-    [
-      [1, 1, 1, 4, 99, 5, 6, 0, 99],
-      [30, 1, 1, 4, 2, 5, 6, 0, 99]
+      [1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
+      [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
     ]
+    // [
+    //   [1, 0, 0, 0, 99],
+    //   [2, 0, 0, 0, 99]
+    // ],
+    // [
+    //   [2, 3, 0, 3, 99],
+    //   [2, 3, 0, 6, 99]
+    // ],
+    // [
+    //   [2, 4, 4, 5, 99],
+    //   [2, 4, 4, 5, 99, 9801]
+    // ],
+    // [
+    //   [1, 1, 1, 4, 99, 5, 6, 0, 99],
+    //   [30, 1, 1, 4, 2, 5, 6, 0, 99]
+    // ]
   ])('execGravityAssistProgram(%a)', (value: number[], expected: number[]) => {
+    test(`returns ${expected}`, async () => {
+      expect.assertions(1);
+      const val$ = await execGravityAssistProgram(value).toPromise();
+      expect(val$).toBe(expected);
+    });
+  });
+});
+
+describe('2: opcode', () => {
+  let program: number[];
+
+  beforeEach(() => {
+    program = [1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50];
+  });
+
+  describe.each`
+    value   | expected
+    ${1}    | ${fns.add}
+    ${2}    | ${fns.multiply}
+    ${99}   | ${fns.exit}
+    ${1202} | ${fns.error}
+  `('getOpFn($value)', ({ value, expected }) => {
     test(`returns ${expected}`, () => {
-      expect(execGravityAssistProgram(value)).toBe(expected);
+      expect(getOpFn(value)).toBe(expected);
+    });
+  });
+
+  test('add', () => {
+    expect(fns.add(1, 3)).toBe(4);
+  });
+
+  test('multiply', () => {
+    expect(fns.multiply(1, 3)).toBe(3);
+  });
+  test('exit', () => {
+    expect(fns.exit(1, 3)).toBe(0);
+  });
+  test('error', () => {
+    expect(fns.error(1, 3)).toBe(1);
+  });
+
+  describe.each`
+    value               | expected
+    ${[1, 9, 10, 3]}    | ${70}
+    ${[2, 3, 11, 0]}    | ${3500}
+    ${[99, 30, 40, 50]} | ${0}
+  `('execOp($value)', ({ value, expected }) => {
+    test(`returns ${expected}`, () => {
+      expect(execOp(value, [...program])).toBe(expected);
     });
   });
 });
--- src/day-2/program-alarm.ts
+++ src/day-2/program-alarm.ts
@@ -1,5 +1,49 @@
-const execGravityAssistProgram = (inputs: number[]): number[] => {
-  return [];
+import { Observable, of } from 'rxjs';
+import { bufferCount, flatMap, groupBy, map, tap } from 'rxjs/operators';
+
+const fns = {
+  add: (x: number, y: number) => x + y,
+  multiply: (x: number, y: number) => x * y,
+  exit: (x: number, y: number) => 0,
+  error: (x: number, y: number) => 1
 };
 
-export { execGravityAssistProgram };
+const getOpFn = (opCode: number) => {
+  switch (opCode) {
+    case 1:
+      return fns.add;
+      break;
+    case 2:
+      return fns.multiply;
+      break;
+    case 99:
+      return fns.exit;
+      break;
+    default:
+      return fns.error;
+      break;
+  }
+};
+
+const execOp = (
+  intCode: [number, number, number, number],
+  program: number[]
+): number => {
+  const [opCode, inputPos1, inputPos2, outputPos] = intCode;
+  const inputs = [program[inputPos1], program[inputPos2]];
+  return getOpFn(opCode)(inputs[0], inputs[1]);
+};
+
+const execGravityAssistProgram = (inputs: number[]): Observable<number[]> => {
+  return of(inputs).pipe(
+    flatMap(n => n),
+    bufferCount(4),
+    map(intcode => {
+      const [opCode, input1, input2, output] = intcode;
+      return getOpFn(opCode);
+    }),
+    tap(console.log)
+  );
+};
+
+export { execGravityAssistProgram, fns, getOpFn, execOp };

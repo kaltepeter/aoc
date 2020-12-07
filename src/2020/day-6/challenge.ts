f8266943b89e7d8c39ea949405cd47c4b8f9ee3b
kaltepeter <kayla.altepeter@datasite.com>
2020-12-06 18:57:31 -0600

    feat(2020/day-6): part II

--- src/2020/day-6/challenge.ts
+++ src/2020/day-6/challenge.ts
@@ -1,4 +1,4 @@
-import { flatten } from 'ramda';
+import { countBy, flatten, toLower } from 'ramda';
 
 const countForGroup = (groupAnswers: string[][]): Set<string> =>
   new Set(flatten(groupAnswers));
@@ -6,4 +6,28 @@ const countForGroup = (groupAnswers: string[][]): Set<string> =>
 const totalYessesForGroup = (groups: string[][][]) =>
   groups.reduce((acc, c) => (acc += countForGroup(c).size), 0);
 
-export { countForGroup, totalYessesForGroup };
+const countAllYessesForGroup = (
+  groupAnswers: string[][]
+): { [key: string]: number } => countBy(toLower, flatten(groupAnswers));
+
+const totalAllYessesForGroup = (
+  numOfPeople: number,
+  groupAnswers: string[][]
+) =>
+  Array.from(
+    new Map(Object.entries(countAllYessesForGroup(groupAnswers)))
+  ).filter(([_, val]) => val === numOfPeople);
+
+const totalAllYessesForAllGroups = (groups: string[][][]) =>
+  groups.reduce(
+    (acc, c) => (acc += totalAllYessesForGroup(c.length, c).length),
+    0
+  );
+
+export {
+  countForGroup,
+  totalYessesForGroup,
+  countAllYessesForGroup,
+  totalAllYessesForGroup,
+  totalAllYessesForAllGroups,
+};

b1395c9501a1a2d1265ee312ba789ff161c4e5c9
Kayla Altepeter <kayla@kaylaaltepeter.com>
2020-12-23 18:06:19 -0600

    feat(2020/day-15): Part I

--- src/2020/day-15/challenge.spec.ts
+++ src/2020/day-15/challenge.spec.ts
@@ -1,3 +1,4 @@
+import { playGame } from './challenge';
 import { inputs, sample } from './inputs';
 
 describe(`Day 15: Rambunctious Recitation`, () => {
@@ -8,7 +9,23 @@ describe(`Day 15: Rambunctious Recitation`, () => {
     expect(inputs[0].length).toBe(6);
   });
 
-  test(``, () => {
-    expect(true).toBe(true);
+  describe.each([
+    [sample[0], 436],
+    [sample[1], 1],
+    [sample[2], 10],
+    [sample[3], 27],
+    [sample[4], 78],
+    [sample[5], 438],
+    [sample[6], 1836],
+  ])(`playGame(%j)`, (startNumbers: number[], expectedResult: number) => {
+    test(`should return ${expectedResult}`, () => {
+      const game = playGame(startNumbers);
+      expect(game).toBe(expectedResult);
+    });
+  });
+
+  test(`playGame(inputs[0])`, () => {
+    const game = playGame(inputs[0]);
+    expect(game).toBe(1259);
   });
 });
--- src/2020/day-15/challenge.ts
+++ src/2020/day-15/challenge.ts
@@ -1,1 +1,26 @@
-export {};
+const playGame = (start: number[]): number => {
+  const turns = [...start];
+  const spokenValues: { [key: string]: number[] } = {};
+  start.forEach((v, idx) => {
+    spokenValues[v] = spokenValues[v] ? [...spokenValues[v], idx] : [idx];
+  });
+  // let lastNum = start[start.length - 1];
+  for (let i = start.length; i < 2020; i++) {
+    let lastNum = turns.slice(-1)[0];
+    const lastNumPositions = spokenValues[lastNum] ? spokenValues[lastNum] : [];
+    const lastTwoNums =
+      lastNumPositions.length > 1 ? lastNumPositions.slice(-2) : -1;
+    if (lastTwoNums !== -1) {
+      lastNum = lastTwoNums[1] - lastTwoNums[0];
+    } else {
+      lastNum = 0;
+    }
+    spokenValues[lastNum] = spokenValues[lastNum]
+      ? [...spokenValues[lastNum], i]
+      : [i];
+    turns.push(lastNum);
+  }
+  return turns.slice(-1)[0];
+};
+
+export { playGame };

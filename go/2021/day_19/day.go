c4640d8e1d7e29ea4c7f112957413684d4e25398
Kayla Altepeter <kayla@kaylaaltepeter.com>
2022-01-01 12:17:48 -0600

    feat(2021): day 19 setup

--- go/2021/day_19/day.go
+++ go/2021/day_19/day.go
@@ -4,30 +4,116 @@ import (
 	"fmt"
 	"ka/m/util"
 	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-func ProcessInput(data *[]string) [][]int {
-	riskData := [][]int{}
-	for _, line := range *data {
-		riskData = append(riskData, util.StringToListOfIntWithSeparator(line, ""))
-	}
-	return riskData
+type Coord struct {
+	X int
+	Y int
+	Z int
 }
 
-func Part1(data *[][]int) int {
+func (c Coord) String() string {
+	return fmt.Sprintf("[%d,%d,%d]", c.X, c.Y, c.Z)
+}
+
+type Scanner struct {
+	Name    int // `default:"27"`
+	Pos     Coord
+	Beacons []Beacon
+}
+
+func NewScanner() *Scanner {
+	return &Scanner{
+		Name:    -1,
+		Beacons: []Beacon{},
+	}
+}
+
+func (s Scanner) String() string {
+	return fmt.Sprintf("[Scanner %d] Pos: %s %d Beacons", s.Name, s.Pos.String(), len(s.Beacons))
+}
+
+func (s *Scanner) Exists() bool {
+	exists := false
+	if s.Name != -1 && len(s.Beacons) > 0 {
+		exists = true
+	}
+	return exists
+}
+
+type Beacon struct {
+	Pos Coord
+}
+
+func (b Beacon) String() string {
+	return fmt.Sprintf("%v", b.Pos)
+}
+
+type ScannerData struct {
+	Scanners []Scanner
+	Beacons  []Beacon
+}
+
+func (sd *ScannerData) String() string {
+	return fmt.Sprintf("Scanners: %v, Beacons: %v", sd.Scanners, sd.Beacons)
+}
+
+const (
+	MAX_DISTANCE = 1000
+)
+
+func ProcessInput(data *[]string) []Scanner {
+	scannerData := []Scanner{}
+	isCoordRegex := regexp.MustCompile(`^\d|-`)
+	var scanner Scanner
+	var beacons []Beacon
+	for _, line := range *data {
+		if strings.Contains(line, "---") {
+			name := -1
+			fmt.Sscanf(line, "--- scanner %d ---", &name)
+			scanner = *NewScanner()
+			beacons = []Beacon{}
+			scanner.Name = name
+		} else if isCoordRegex.MatchString(line) {
+			coords := strings.Split(line, ",")
+			x, _ := strconv.ParseInt(coords[0], 10, 64)
+			y, _ := strconv.ParseInt(coords[1], 10, 64)
+			z, _ := strconv.ParseInt(coords[2], 10, 64)
+			beacons = append(beacons, Beacon{
+				Pos: Coord{
+					X: int(x),
+					Y: int(y),
+					Z: int(z),
+				},
+			})
+		} else if line == "" {
+			scanner.Beacons = beacons
+			scannerData = append(scannerData, scanner)
+		}
+	}
+	return scannerData
+}
+
+func Part1(data *[]Scanner) int {
+	for _, s := range *data {
+		fmt.Println(s)
+	}
 	return 0
 }
 
-func Part2(data *[][]int) int {
+func Part2(data *[]Scanner) int {
 	return 0
 }
 
 func main() {
-	input := filepath.Join("2021", "day_xx", "raw-input.txt")
+	input := filepath.Join("2021", "day_19", "raw-input.txt")
 	inputData := util.ParseInput(input)
 	riskData := ProcessInput(&inputData)
 	p1Result := Part1(&riskData)
-	fmt.Printf("Part I: the lowest risk path level is = %v\n", p1Result)
+	fmt.Printf("Part I: the number of beacons is = %v\n", p1Result)
 	if p1Result != 562 {
 		panic("FAILED on Part I")
 	}
--- go/2021/day_19/day_test.go
+++ go/2021/day_19/day_test.go
@@ -6,23 +6,38 @@ import (
 	"testing"
 )
 
-func setupTests(filePath string) [][]int {
+func setupTests(filePath string) []Scanner {
 	inputExample := filepath.Join(filePath)
 	inputData := util.ParseInput(inputExample)
-	riskData := ProcessInput(&inputData)
-	return riskData
+	scannerData := ProcessInput(&inputData)
+	return scannerData
+}
+
+func TestProcessInput(t *testing.T) {
+	input := setupTests("example.txt")
+	got := len(input)
+	want := 4
+	if got != want {
+		t.Errorf(`ProcessInput should return %v items, got: %v`, want, got)
+	}
+
+	gotBeaconCount := len(input[0].Beacons)
+	wantBeaconCount := 25
+	if gotBeaconCount != wantBeaconCount {
+		t.Errorf(`ProcessInput should return %v beacons for scanner 0, got: %v`, wantBeaconCount, gotBeaconCount)
+	}
 }
 
 func TestPartI(t *testing.T) {
 	input := setupTests("example.txt")
 	got := Part1(&input)
-	want := 40
+	want := 79
 	if got != want {
 		t.Errorf(`Part I should return %v, got %v`, want, got)
 	}
 }
 
-func TestPartII(t *testing.T) {
+func SkipTestPartII(t *testing.T) {
 	input := setupTests("example.txt")
 	got := Part2(&input)
 	want := 315

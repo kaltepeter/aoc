2b694a89bf944cb090bb81a632b9b5cc8f24786a
Kayla Altepeter <kayla@kaylaaltepeter.com>
2022-12-01 14:58:56 -0600

    feat(2022): day 1

---
+++ python/2022/day_1/day.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+from typing import Dict, List, NewType, TypedDict
+
+base_path = Path(__file__).parent
+
+Elf = TypedDict("Elf", {"items": List[int], "calories": int})
+Elves = List[Elf]
+
+
+def calc_calories(elf: Elf) -> int:
+    return sum(elf["items"])
+
+
+def process_input(file: str) -> Elves:
+    with open(file) as reader:
+        elf_data = reader.read().strip().split("\n\n")
+        elf_list = [e.split("\n") for e in elf_data]
+        return [{"items": [int(e) for e in elf]} for elf in elf_list]
+
+
+def part_1(elves: Elves) -> int:
+    return max([calc_calories(elf) for elf in elves])
+
+
+def part_2(elves: Elves) -> int:
+    for elf in elves:
+        elf["calories"] = calc_calories(elf)
+    top_three = sorted(elves, key=lambda elf: elf["calories"], reverse=True)[:3]
+    return sum(d["calories"] for d in top_three)
+
+
+def main():
+    elves = process_input(os.path.join(base_path, "input.txt"))
+
+    print(f"Part I: {part_1(elves)} is the most calories")
+
+    print(
+        f"Part II: {part_2(elves)} is the sum of the top three highest calorie counts"
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/2022/day_1/day_test.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+
+from day import process_input, calc_calories, part_1, part_2
+
+base_path = Path(__file__).parent
+
+
+def test_process_input():
+    assert process_input(os.path.join(base_path, "example.txt")) == [
+        {"items": [1000, 2000, 3000]},
+        {
+            "items": [4000],
+        },
+        {"items": [5000, 6000]},
+        {"items": [7000, 8000, 9000]},
+        {"items": [10000]},
+    ]
+
+
+def test_calc_calories():
+    assert calc_calories({"items": [1000, 2000, 3000]}) == 6000
+
+
+def test_part_1():
+    elves = process_input(os.path.join(base_path, "example.txt"))
+    assert part_1(elves) == 24000
+
+
+def test_part_2():
+    elves = process_input(os.path.join(base_path, "example.txt"))
+    assert part_2(elves) == 45000

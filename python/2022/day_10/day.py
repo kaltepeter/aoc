c01d4eba504176af49cc06583c771b90b4bb3ba6
Kayla Altepeter <kayla@kaylaaltepeter.com>
2022-12-18 22:37:49 -0600

    feat(2022): day 10 part II

--- python/2022/day_10/day.py
+++ python/2022/day_10/day.py
@@ -1,6 +1,8 @@
+from copy import copy
 import os
 from pathlib import Path
 from typing import List, Literal
+from PIL import Image
 
 base_path = Path(__file__).parent
 
@@ -33,20 +35,20 @@ def get_signal_strength(state: dict[int, int]) -> int:
     return sum(values)
 
 
-def part_1(instructions: Instructions) -> int:
+def get_state(instructions: Instructions) -> dict[int, int]:
     state = {}
     cycles_to_run = {}
     inst = None
-    for idx in range(1, 222):
+    for idx in range(1, 240):
         cycle = idx
 
         # init register
         register = state[cycle - 1] if cycle - 1 in state else 1
         state[cycle] = register
 
-        print(
-            f"cycle: {cycle} ({inst}, {register}) cycles_to_run: {cycles_to_run} inst_left: {len(instructions)}"
-        )
+        # print(
+        #     f"cycle: {cycle} ({inst}, {register}) cycles_to_run: {cycles_to_run} inst_left: {len(instructions)}"
+        # )
 
         if cycle in cycles_to_run:
             register += cycles_to_run[cycle]
@@ -65,28 +67,54 @@ def part_1(instructions: Instructions) -> int:
                 case _:
                     raise ValueError(f"Unknown instruction {inst[0]}")
 
+    return state
+
+
+def part_1(instructions: Instructions) -> int:
+    state = get_state(instructions)
+
     # print(state)
-    for c in interesting_cycles:
-        print(f"{c}: {state[c]}")
+    # for c in interesting_cycles:
+    #     print(f"{c}: {state[c]}")
 
     return get_signal_strength(state)
 
 
-def part_2(instructions: Instructions) -> int:
-    register = 1
-    return register
+def part_2(instructions: Instructions) -> str:
+    state = get_state(instructions)
+    # vals = set(state.values())
+    # print(vals)
+    out = ""
+    for i in range(0, 240):
+        register = state[i] if i in state else 0
+        # print(f"register: {register} i: {i}")
+
+        rowI = i % 40
+
+        if rowI == 0:
+            out += "\n"
+
+        if rowI in range(register - 1, register + 2):
+            out += "#"
+            # out += "█"
+        else:
+            out += "."
+            # out += " "
+
+    print(out.strip())
+    return out.strip()
 
 
 def main():
     instructions = process_input(os.path.join(base_path, "input.txt"))
 
-    part1_answer = part_1(instructions)
-    print(f"Part I: {part1_answer} signal strength")
+    part1_answer = part_1(copy(instructions))
+    print(f"Part I: {part1_answer} signal strength\n")
     assert part1_answer == 16880
 
-    part2_answer = part_2(instructions)
-    print(f"Part II: {part2_answer} signal strength")
-    assert part2_answer == 0
+    part2_answer = part_2(copy(instructions))
+    print(f"Part II: {len(part2_answer)} length of chars\n")
+    assert len(part2_answer) == 245
 
 
 if __name__ == "__main__":

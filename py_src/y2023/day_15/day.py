586bba1d37a4d37a0c83c019402dc43f94a3baab
Kayla Altepeter <kayla@kaylaaltepeter.com>
2023-12-18 12:35:54 -0600

    feat92023): day 15 part I

--- py_src/y2023/day_15/day.py
+++ py_src/y2023/day_15/day.py
@@ -11,12 +11,29 @@ InputData = List[str]
 
 def process_input(file: str) -> Generator[InputData, None, None]:
     with open(file, "r") as reader:
-        for pairs in reader.read().split("\n\n"):
-            yield list(map(lambda val: val.strip(), pairs.splitlines()))
+        for blocks in reader.read().split("\n\n"):
+            res = list(map(lambda val: val.strip().split(","), blocks.splitlines()))
+            assert len(res) == 1
+            yield res[0]
+
+
+def holiday_hash(val: str) -> int:
+    result = 0
+    for char in val:
+        result += ord(char)
+        result *= 17
+        result = result % 256
+
+    assert result > -1 and result < 256
+    return result
 
 
 def part_1(data: InputData) -> int:
-    return 0
+    results = []
+    for seq in data:
+        results.append(holiday_hash(seq))
+
+    return sum(results)
 
 
 def part_2(data: InputData) -> int:
@@ -28,7 +45,7 @@ def main():
 
     part1_answer = part_1(deepcopy(pi))
     print(f"Part I: {part1_answer} \n")
-    assert part1_answer == 0
+    assert part1_answer == 501680
 
     part2_answer = part_2(deepcopy(pi))
     print(f"Part II: {part2_answer} \n")
---
+++ py_src/y2023/day_15/day_test.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+
+import pytest
+from .day import part_1, part_2, process_input, holiday_hash
+
+base_path = Path(__file__).parent
+
+
+@pytest.fixture()
+def example_data():
+    return process_input(os.path.join(base_path, "example.txt"))
+
+
+def test_holiday_hash():
+    assert holiday_hash("HASH") == 52
+
+
+def test_process_input(example_data):
+    assert next(example_data) == [
+        "rn=1",
+        "cm-",
+        "qp=3",
+        "cm=2",
+        "qp-",
+        "pc=4",
+        "ot=9",
+        "ab=5",
+        "pc-",
+        "pc=6",
+        "ot=7",
+    ]
+
+
+def test_part_1(example_data):
+    assert part_1(next(example_data)) == 1320
+
+
+def test_part_2(example_data):
+    assert part_2(next(example_data)) == 0

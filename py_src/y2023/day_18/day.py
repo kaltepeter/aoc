3e3882213d6341ff037b839278cffd07f124f090
Kayla Altepeter <kayla@kaylaaltepeter.com>
2024-01-01 11:06:15 -0600

    feat(2023): day 18 part II

--- py_src/y2023/day_18/day.py
+++ py_src/y2023/day_18/day.py
@@ -3,9 +3,11 @@ import os
 import pathlib
 from typing import Generator, List
 from matplotlib.path import Path
+from matplotlib.axes import Axes
 import matplotlib.patches as patches
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.transforms as transforms
 
 base_path = pathlib.Path(__file__).parent
 
@@ -18,6 +20,13 @@ directions: dict[str, Direction] = {
     "L": (-1.0, 0.0),
     "U": (0.0, -1.0),
     "D": (0.0, 1.0),
+}
+
+digit_to_directions: dict[int, str] = {
+    0: "R",
+    1: "D",
+    2: "L",
+    3: "U",
 }
 
 
@@ -51,15 +60,84 @@ def process_input(file: str) -> Generator[InputData, None, None]:
             ]
 
 
+def convert_hex(data: InputData) -> List[tuple[str, float]]:
+    return [
+        (digit_to_directions[int(hex[-1])], int(hex[1:6], 16)) for _, _, hex in data
+    ]
+
+
+def visualize(path: Path, min_x=0, min_y=0, max_x=1, max_y=1, scale=1) -> None:
+    border_size = 10
+    scale_transform = transforms.Affine2D().scale(scale)
+    fig, ax = plt.subplots()
+    patch = patches.PathPatch(path, facecolor="orange", edgecolor="red", lw=2)
+    patch.set_transform(scale_transform + ax.transData)
+    ax.add_patch(patch)
+
+    ax.set_xlim(min_x - border_size, max_x + border_size)
+    ax.set_ylim(min_y - border_size, max_y + border_size)
+    ax.invert_yaxis()
+
+    plt.show()
+
+
+def calculate_points(path: Path, min_x=0, min_y=0, max_x=1, max_y=1, scale=1) -> int:
+    count = 0
+    for y in range(min_y - 1, max_y + 1):
+        for x in range(min_x - 1, max_x + 1):
+            # if inside or edge
+            radius = 0.1 / scale
+            if path.contains_point((x, y)) or path.contains_point(
+                (x, y), radius=radius
+            ):
+                count += 1
+
+    return count
+
+
+# https://artofproblemsolving.com/wiki/index.php?title=Shoelace_Theorem
+def calculate_area(vertices: List[Location]) -> int:
+    vertices = list(reversed(vertices))
+    # handle overflow
+    return (
+        abs(
+            sum(
+                vertices[i][0]
+                * (vertices[i - 1][1] - vertices[(i + 1) % len(vertices)][1])
+                for i in range(len(vertices))
+            )
+        )
+        // 2
+    )
+    # ccw = list(reversed(vertices))
+    # count = len(ccw)
+    # sum1 = 0
+    # sum2 = 0
+
+    # for i in range(0, count - 1):
+    #     sum1 += ccw[i][0] * ccw[i + 1][1]
+    #     sum2 += ccw[i][1] * ccw[i + 1][0]
+
+    #     sum1 += ccw[count - 1][0] * ccw[0][1]
+    #     sum2 += ccw[0][0] * ccw[count - 1][1]
+
+    # return abs(sum1 - sum2) / 2
+
+
+# really good walkthrough: https://www.youtube.com/watch?v=UNimgm_ogrw
+def calculate_points_picks(area: int, boundary_points: int) -> int:
+    return area + (boundary_points // 2) + 1
+
+
 def part_1(data: InputData) -> int:
-    rows = len(data)
-    cols = len(data[0])
     codes = [Path.MOVETO]
     current_position: Direction = (0.0, 0.0)
     vertices = [current_position]
+    boundary_points = 0
 
-    for d, amount, color in data:
+    for d, amount, _ in data:
         direction = directions[d]
+        boundary_points += amount
         move_amount = (direction[0] * amount, direction[1] * amount)
         current_position = (
             current_position[0] + move_amount[0],
@@ -68,33 +146,47 @@ def part_1(data: InputData) -> int:
         vertices.append(current_position)
         codes.append(Path.LINETO)
 
+    x_coords, y_coords = zip(*vertices)
+
+    min_x = int(min(x_coords))
+    max_x = int(max(x_coords))
+    min_y = int(min(y_coords))
+    max_y = int(max(y_coords))
+
+    # min_x = int(min([v[0] for v in vertices]))
+    # max_x = int(max([v[0] for v in vertices]))
+    # min_y = int(min([v[1] for v in vertices]))
+    # max_y = int(max([v[1] for v in vertices]))
+
     # Visual
-    path: Path = Path(vertices, codes)
-    fig, ax = plt.subplots()
-    patch = patches.PathPatch(path, facecolor="orange", edgecolor="red", lw=2)
-    ax.add_patch(patch)
-    min_x = int(min([v[0] for v in vertices]))
-    max_x = int(max([v[0] for v in vertices]))
-    min_y = int(min([v[1] for v in vertices]))
-    max_y = int(max([v[1] for v in vertices]))
-    ax.set_xlim(-2, max_x + 2)
-    ax.set_ylim(-2, max_y + 2)
-    ax.invert_yaxis()
+    # path: Path = Path(vertices, codes)
+    # visualize(path, min_x=min_x, min_y=min_y, max_x=max_x, max_y=max_y)
+    # return calculate_points(path, min_x=min_x, min_y=min_y, max_x=max_x, max_y=max_y)
+    area = calculate_area(vertices)
 
-    # plt.show()
-
-    count = 0
-    for y in range(min_y - 1, max_y + 1):
-        for x in range(min_x - 1, max_x + 1):
-            # if inside or edge
-            if path.contains_point((x, y)) or path.contains_point((x, y), radius=0.1):
-                count += 1
-
-    return count
+    return int(calculate_points_picks(area, boundary_points))
 
 
 def part_2(data: InputData) -> int:
-    return 0
+    current_position: Direction = (0.0, 0.0)
+    vertices = [current_position]
+
+    converted_hex = convert_hex(data)
+
+    boundary_points = 0
+    for d, amount in converted_hex:
+        direction = directions[d]
+        boundary_points += amount
+        move_amount = (direction[0] * amount, direction[1] * amount)
+        current_position = (
+            current_position[0] + move_amount[0],
+            current_position[1] + move_amount[1],
+        )
+        vertices.append(current_position)
+
+    area = calculate_area(vertices)
+
+    return int(calculate_points_picks(area, boundary_points))
 
 
 def main():
@@ -106,7 +198,7 @@ def main():
 
     part2_answer = part_2(deepcopy(pi))
     print(f"Part II: {part2_answer} \n")
-    assert part2_answer == 0
+    assert part2_answer == 48020869073824
 
 
 if __name__ == "__main__":
--- py_src/y2023/day_18/day_test.py
+++ py_src/y2023/day_18/day_test.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 import pytest
-from .day import part_1, part_2, process_input
+from .day import part_1, part_2, process_input, convert_hex
 
 base_path = Path(__file__).parent
 
@@ -31,9 +31,28 @@ def test_process_input(example_data):
     ]
 
 
+def test_hex_codes(example_data):
+    assert convert_hex(next(example_data)) == [
+        ("R", 461937.0),
+        ("D", 56407.0),
+        ("R", 356671.0),
+        ("D", 863240.0),
+        ("R", 367720.0),
+        ("D", 266681.0),
+        ("L", 577262.0),
+        ("U", 829975.0),
+        ("L", 112010.0),
+        ("D", 829975.0),
+        ("L", 491645.0),
+        ("U", 686074.0),
+        ("L", 5411.0),
+        ("U", 500254.0),
+    ]
+
+
 def test_part_1(example_data):
     assert part_1(next(example_data)) == 62
 
 
 def test_part_2(example_data):
-    assert part_2(next(example_data)) == 0
+    assert part_2(next(example_data)) == 952408144115
